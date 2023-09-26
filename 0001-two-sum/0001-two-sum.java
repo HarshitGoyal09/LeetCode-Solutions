039818b1c27b7ed5355eae545b5f127939a98818
Harshit Goyal <47056897+HarshitGoyal09@users.noreply.github.com>
2023-09-26 11:22:41 +0530

    Time: 2 ms (82.57%), Space: 43.5 MB (82.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,31 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
     int n = nums.length;
-    int[] arr = new int[2];
-      for(int i=0;i<n;i++){
-          for(int j=i+1;j<n;j++){
-              if(nums[i] + nums[j] == target)
-              {
-                  arr[0] =i;
-                  arr[1] =j;
-              }
-          }
-     }
-     return arr; 
-    }
+    // Brute T.C- O(n^2)
+    // int[] arr = new int[2];
+    //   for(int i=0;i<n;i++){
+    //       for(int j=i+1;j<n;j++){
+    //           if(nums[i] + nums[j] == target)
+    //           {
+    //               arr[0] =i;
+    //               arr[1] =j;
+    //           }
+    //       }
+    //  }
+    //  return arr;
+
+    //  Better T.C - O(n)
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for(int i =0;i<n;i++){
+            int complement = target - nums[i];
+            if(hash.containsKey(complement)){
+                return new int[]{hash.get(complement),i};
+            }
+            hash.put(nums[i],i);
+        }
+        return new int[]{};
+}
+
+    // T.C = O(n2)
 } 
