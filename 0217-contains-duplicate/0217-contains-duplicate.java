f50da4ad71978e8cb87672c979822564eeacb24a
Harshit Goyal <47056897+HarshitGoyal09@users.noreply.github.com>
2023-12-19 20:10:07 +0530

    Time: 21 ms (8.62%), Space: 57.7 MB (12.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //Approach-1
+        boolean flag = false;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1])
-                return true;
+        for(int i=1;i<=nums.length-1;i++){
+            if(nums[i-1] == nums[i]){
+                flag = true;
+                break;
+            }
         }
-    return false;
+        return flag;
     }
 }
